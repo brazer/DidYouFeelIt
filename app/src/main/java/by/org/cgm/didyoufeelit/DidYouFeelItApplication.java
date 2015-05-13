36600a5ee7118e49aad0e02732c36899c12ff4ba
brazer <salanevich.anatol@gmail.com>
2015-05-13 16:18:19 +0300

    added LeakCanary lib

--- app/src/main/java/by/org/cgm/didyoufeelit/DidYouFeelItApplication.java
+++ app/src/main/java/by/org/cgm/didyoufeelit/DidYouFeelItApplication.java
@@ -3,6 +3,8 @@ package by.org.cgm.didyoufeelit;
 import android.app.Application;
 import android.content.Context;
 
+import com.squareup.leakcanary.LeakCanary;
+
 /**
  * Author: Anatol Salanevich
  * Date: 08.05.2015
@@ -13,6 +15,12 @@ public class DidYouFeelItApplication extends Application {
 
     public DidYouFeelItApplication() {
         mContext = this;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LeakCanary.install(this);
     }
 
     public static Context getContext() {
