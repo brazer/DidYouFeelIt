b6fe108f67690015936317fad8e09a3310d3731e
brazer <salanevich.anatol@gmail.com>
2015-07-23 11:40:03 +0300

    changed sensitivity point in settings

--- app/src/main/java/by/org/cgm/didyoufeelit/activities/SettingsActivity.java
+++ app/src/main/java/by/org/cgm/didyoufeelit/activities/SettingsActivity.java
@@ -60,8 +60,6 @@ public class SettingsActivity extends PreferenceActivity {
         // Add 'general' preferences.
         addPreferencesFromResource(R.xml.pref_detector);
 
-        bindPreferenceSummaryToValue(findPreference(getString(R.string.detector_sensitivity)));
-
         // Add 'notifications' preferences, and a corresponding header.
         PreferenceCategory fakeHeader = new PreferenceCategory(this);
         fakeHeader.setTitle(R.string.pref_header_user);
@@ -186,7 +184,6 @@ public class SettingsActivity extends PreferenceActivity {
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
-            bindPreferenceSummaryToValue(findPreference(getString(R.string.detector_sensitivity)));
         }
     }
 
